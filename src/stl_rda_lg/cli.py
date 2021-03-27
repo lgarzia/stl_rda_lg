295587e277d6e7010f61daa7fdee189780f277f8
Luke Garzia <lgarzia@yahoo.com>
2021-03-27 17:19:29 -0500

    Initial Commit

---
+++ src/stl_rda_lg/cli.py
@@ -0,0 +1,25 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.callback()  ##not sure what this does yet
+def callback():
+    """
+    Package built to explore STL Data Region Alliance Data
+    """
+
+
+@app.command()
+def run():
+    import streamlit.cli as stcli
+    import sys
+    import os
+
+    fdir = os.path.dirname(__file__)
+    fpath = os.path.join(fdir, "strlit", "main.py")
+    """ Calls the streamlit application"""
+    typer.echo("test command")
+    sys.argv = ["streamlit", "run", fpath]
+    typer.echo(fpath)
+    sys.exit(stcli.main())
---
+++ src/stl_rda_lg/strlit/main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from stl_rda_lg.api import GET_DATA
+
+st.write("hello world")
+st.write(GET_DATA)
